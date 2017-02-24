9fe755bce9da8ab1dba3da96070b09be21a0fc99
Patrick Kliesner <Patrick Kliesner>
2017-02-23 23:18:34 -0600

    Catalog Update

--- template.js
+++ template.js
@@ -1,5 +1,3 @@
-<<<<<<< HEAD
-
 /** @module template
  */
  
@@ -37,48 +35,3 @@
 	 });
 	 return html;
  }
- 
- 
-=======
-
-/** @module template
- */
- 
- module.exports = {
-	 render: render,
-	 loadDir: loadDir
- }
- 
- var fs = require('fs');
- var templates = {}
- 
- /** @function loadDir
-  * Loads a directory of templates
-  * @param {string} directory - the directory to load
-  */
- function loadDir(directory){
-	 var dir = fs.readdirSync(directory);
-	 dir.forEach(function(file){
-		 var path = directory + '/' + file;
-		 var stats = fs.statSync(path);
-		 if (stats.isFile()){
-			 templates[file] = fs.readFileSync(path).toString();
-		 }
-	 });
- }
- 
- /** @function render
-  * Renders a template with embedded javascript
-  * @param {string} templateName = the template to render
-  * @param {context}...
-  */
- function render(templateName, context){
-	 return templates[templateName].replace(/<%=(.+?) %>/g, function(match, js){
-		return eval("var context = " + JSON.stringify(context) + ";" + js);
-	 });
-	 return html;
- }
- 
- 
->>>>>>> 500314139beb433d2341df183ce8ce856d6d86bb
- 
